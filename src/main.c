8c3fb76564a0817ca0df8ca7520f69beb038bb0a
GMiranda21ML <gabriel.mucarbel.lima@gmail.com>
2025-04-29 11:12:28 -0300

    fazendo movimentação + gravidade + chao de teste

--- src/main.c
+++ src/main.c
@@ -7,39 +7,74 @@ int main() {
 
     PlayerSprites sprites = LoadPlayerSprites("assets/sprites/player/player.json");
 
-    Vector2 position = {100, 300};
-    float speed = 2.5f;
+    Rectangle ground = { 0, 550, 800, 100 }; // chão visível e com altura
+
+    Vector2 position = {400, 500};
+    Vector2 velocity = {0, 0};
+    float gravity = 900.0f;
+    float jumpForce = -400.0f;
+    bool isOnGround = false;
+
+    float speed = 200.0f;
     float timer = 0;
     int frame = 0;
-    int facing = 1; // 1 = right, -1 = left
+    int facing = 1; // 1 = direita, -1 = esquerda
 
     while (!WindowShouldClose()) {
         float dt = GetFrameTime();
+        bool moving = false;
 
-        // Movimento
-        if (IsKeyDown(KEY_RIGHT)) {
-            position.x += speed;
+        // Entrada horizontal
+        if (IsKeyDown(KEY_D)) {
+            position.x += speed * dt;
             facing = 1;
-            timer += dt;
-        } else if (IsKeyDown(KEY_LEFT)) {
-            position.x -= speed;
+            moving = true;
+        } else if (IsKeyDown(KEY_A)) {
+            position.x -= speed * dt;
             facing = -1;
+            moving = true;
+        }
+
+        // Gravidade
+        velocity.y += gravity * dt;
+        position.y += velocity.y * dt;
+
+        // Colisão com o chão
+        float playerHeight = (float)sprites.walk_right.frames[0].height;
+        if (position.y + playerHeight >= ground.y) {
+            position.y = ground.y - playerHeight;
+            velocity.y = 0;
+            isOnGround = true;
+        } else {
+            isOnGround = false;
+        }
+
+        // Pulo
+        if (isOnGround && IsKeyPressed(KEY_W)) {
+            velocity.y = jumpForce;
+            isOnGround = false;
+        }
+
+        // Animação
+        if (moving) {
             timer += dt;
+            if (timer > sprites.frame_change) {
+                frame = (frame + 1) % sprites.walk_right.frame_count;
+                timer = 0;
+            }
         } else {
             timer = 0;
             frame = 0;
-        }
-
-        // Trocar frame
-        if (timer > sprites.frame_change) {
-            frame = (frame + 1) % sprites.walk_right.frame_count;
-            timer = 0;
         }
 
         // Desenhar
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
+        // Desenha chão
+        DrawRectangleRec(ground, DARKGREEN);
+
+        // Sprite atual
         Texture2D current =
             (facing == 1) ? sprites.walk_right.frames[frame] :
                             sprites.walk_left.frames[frame];
