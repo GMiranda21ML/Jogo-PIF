e92e0fd5ee0ae2c94d4f2846db3400bdae32faf9
GMiranda21ML <gabriel.mucarbel.lima@gmail.com>
2025-04-30 08:58:32 -0300

    colocando sprites e plataformas

--- src/main.c
+++ src/main.c
@@ -1,18 +1,26 @@
 #include "raylib.h"
 #include "sprites.h"
 
+#define PLATFORM_COUNT 3
+
 int main() {
     InitWindow(800, 600, "Player Animation");
     SetTargetFPS(60);
 
     PlayerSprites sprites = LoadPlayerSprites("assets/sprites/player/player.json");
 
-    Rectangle ground = { 0, 550, 800, 100 }; // chão visível e com altura
+    Rectangle ground = { 0, 550, 800, 100 }; // chão visível
+
+    Rectangle platforms[PLATFORM_COUNT] = {
+        {200, 450, 150, 20},
+        {450, 350, 150, 20},
+        {300, 250, 100, 20}
+    };
 
     Vector2 position = {400, 500};
     Vector2 velocity = {0, 0};
     float gravity = 900.0f;
-    float jumpForce = -400.0f;
+    float jumpForce = -450.0f;
     bool isOnGround = false;
 
     float speed = 200.0f;
@@ -35,18 +43,37 @@ int main() {
             moving = true;
         }
 
-        // Gravidade
+        // Aplicar gravidade
         velocity.y += gravity * dt;
         position.y += velocity.y * dt;
 
-        // Colisão com o chão
+        // Verificar colisão com plataformas
+        isOnGround = false;
         float playerHeight = (float)sprites.walk_right.frames[0].height;
+        Texture2D current =
+            (facing == 1) ? sprites.walk_right.frames[frame] :
+                            sprites.walk_left.frames[frame];
+
+        for (int i = 0; i < PLATFORM_COUNT; i++) {
+            Rectangle plat = platforms[i];
+
+            if (velocity.y >= 0 &&
+                position.y + playerHeight >= plat.y &&
+                position.y + playerHeight - velocity.y * dt <= plat.y &&
+                position.x + current.width > plat.x &&
+                position.x < plat.x + plat.width) {
+
+                position.y = plat.y - playerHeight;
+                velocity.y = 0;
+                isOnGround = true;
+            }
+        }
+
+        // Verificar colisão com o chão
         if (position.y + playerHeight >= ground.y) {
             position.y = ground.y - playerHeight;
             velocity.y = 0;
             isOnGround = true;
-        } else {
-            isOnGround = false;
         }
 
         // Pulo
@@ -67,18 +94,19 @@ int main() {
             frame = 0;
         }
 
-        // Desenhar
+        // Desenho
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
 
-        // Desenha chão
+        // Desenhar chão
         DrawRectangleRec(ground, DARKGREEN);
 
-        // Sprite atual
-        Texture2D current =
-            (facing == 1) ? sprites.walk_right.frames[frame] :
-                            sprites.walk_left.frames[frame];
+        // Desenhar plataformas
+        for (int i = 0; i < PLATFORM_COUNT; i++) {
+            DrawRectangleRec(platforms[i], GRAY);
+        }
 
+        // Desenhar jogador
         DrawTexture(current, (int)position.x, (int)position.y, WHITE);
 
         EndDrawing();
